5b31287f98bf3462079a74cdf2c8a1490dd0d437
MarcCoet <marcoet@gmail.com>
2017-05-28 15:12:20 +0200

    added metadescription and removed unused pictures

--- src/utils/metadata.js
+++ src/utils/metadata.js
@@ -3,7 +3,7 @@ export const meta = {
   lang: "fr",
   name: "Magali Silvestre",
   title: "Coaching créativité, vie et entrepreneuriat",
-  description: ""
+  description: "J’accompagne les personnes qui veulent atteindre leurs objectifs dans différents domaines de leur vie afin de trouver leurs réponses et de leur permettre de vivre plus consciemment, joyeusement, sereinement et de se reconnecter à qui ils sont."
 }
 
 // TITLE TAG - Optimal format
